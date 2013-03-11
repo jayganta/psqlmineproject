102cb14d304159299698287ddc340162a97b42b3
ohwow <jayganta@me.com>
2013-03-11 11:14:21 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Psqlmineproject::Application.config.secret_token = '9f1f57e7e157d891eadd8ac83b31bc92a1bf3f9e7d8010d1dbc0b9e32f89000de0b189cec8b6952f09c0373c53d7a5f0b21d0fd10a88c84b0410d3b964e1ea2e'
